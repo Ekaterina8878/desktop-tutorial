3f98089b4e1e7d244f58bbadb72e6d810c8bced9
Ekaterina <kobro.kzn@gmail.com>
2024-12-19 14:36:16 +0300

    Add files via upload

--- функции.py
+++ функции.py
@@ -1,14 +1,12 @@
-def get_matrix (n, m, value) :
-    matrix = []
-    for i in range(n):
-        matrix.append([])
+def get_matrix (n, m, value) : # oбъявили функцию с параметрами
+    matrix = [] # создали пустой список внутри функции
+    for i in range(n): # написали первый цикл для n
+        matrix.append([]) # добавили пустой список в список matrix
 
-        for j in range(m):
-            matrix[i].append(value)
+
+        for j in range(m): # внутренний цикл для m
+            matrix[i].append(value) #пополнили ранее пустой список значениями value
     print(matrix)
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
