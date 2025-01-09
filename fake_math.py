f15061c22b3aaf58c1cad26cb39bd301441d8266
Ekaterina <kobro.kzn@gmail.com>
2025-01-09 19:26:26 +0300

    module_4_1

---
+++ fake_math.py
@@ -0,0 +1,7 @@
+def divide (first, second):
+
+    if second != 0:
+        i = first/second
+        return i
+    else:
+        return 'Ошибка'
---
+++ true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+def divide(first, second):
+    if second != 0:
+        i = first / second
+        return i
+    else:
+        return inf
+
