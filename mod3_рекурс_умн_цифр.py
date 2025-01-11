1b89420a1c707e4b0e619cf64cf92732fd60d311
Ekaterina <kobro.kzn@gmail.com>
2025-01-11 15:16:43 +0300

    modul3_rekurs

--- mod3_рекурс_умн_цифр.py
+++ mod3_рекурс_умн_цифр.py
@@ -1,11 +1,16 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-    if len(str_number)>1:
-        first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1:]))
 
+
+    if len(str_number) > 1:
+        first = int(str_number[0])
+        if first != 0:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+            return get_multiplied_digits(int(str_number[1:]))
     else:
-        return int(str_number)
+        return int(str_number) if int(str_number) != 0 else 1  # Для нуля возвращаем 1
+
 
 result = get_multiplied_digits(40203)
 result2 = get_multiplied_digits(402030)
