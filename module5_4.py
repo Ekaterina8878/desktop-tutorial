c34097a5e9a90e5ac4d9a8f8fdafce15e4f96455
Ekaterina <kobro.kzn@gmail.com>
2025-01-15 18:14:56 +0300

    module5_4

--- module5_4.py
+++ module5_4.py
@@ -1,16 +1,26 @@
 class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        house_name = args[0]
+        cls.houses_history.append(house_name)
+        return super().__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f"{self.name} снесён, но он останется в истории")
 
     def go_to(self, new_floor):
         if new_floor < 1 or new_floor > self.number_of_floors:
             print(f'Такого этажа не существует')
         else:
-            for floor in range(1,new_floor + 1):
+            for floor in range(1, new_floor + 1):
                 print(floor)
 
-    def __len__ (self):
+    def __len__(self):
         return self.number_of_floors
 
     def __str__(self):
@@ -70,23 +80,14 @@ class House:
         return self.__add__(value)
 
 
-
 h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
 h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
 
-print(h1)
-print(h2)
-
-print(h1 == h2) # __eq__
-h1 = h1 + 10 # __add__
-print(h1)
-print(h1 == h2)
-h1 += 10 # __iadd__
-print(h1)
-h2 = 10 + h2 # __radd__
-print(h2)
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+# Удаление объектов
+del h2
+del h3
+print(House.houses_history)
